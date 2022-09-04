137cf156f5a7908455e398caaf44d5aa7f142655
Sean Ormiston <sean.ormiston@unbabel.com>
2022-09-04 11:09:03 +0100

    abstracts AuthGuard as a custom class implementation (implementing JWT strat)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from '../auth/guard';
 import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtGuard)
   @Get('me')
   getMe(@Req() req: Request) {
     return req.user;
